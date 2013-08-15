7e761ff1fca9627d9b580fd4334a58e09b242351
Shevchik <Shev4ik.den@gmail.com>
2013-08-15 07:13:09 +0400

    Create empty project

---
+++ src/tntrun/TNTRun.java
@@ -0,0 +1,19 @@
+package tntrun;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TNTRun extends JavaPlugin {
+
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+	
+}
