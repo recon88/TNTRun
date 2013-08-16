286a7238f468506a190fe6ca83cef30a7a54415c
Shevchik <Shev4ik.den@gmail.com>
2013-08-16 11:23:41 +0400

    Create empty game command Listener.

---
+++ src/tntrun/commands/GameCommands.java
@@ -0,0 +1,16 @@
+package tntrun.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GameCommands implements CommandExecutor{
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label,
+			String[] args) {
+
+		return false;
+	}
+
+}
