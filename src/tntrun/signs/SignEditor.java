3e5fe3c9f41060bef6f92838030a8d97981cf0a5
Shevchik <Shev4ik.den@gmail.com>
2013-10-28 00:53:35 +0400

    Remove unused import

--- src/tntrun/signs/SignEditor.java
+++ src/tntrun/signs/SignEditor.java
@@ -8,7 +8,6 @@ import java.util.Hashtable;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.configuration.ConfigurationSection;
