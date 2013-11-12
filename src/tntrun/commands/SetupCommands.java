f70e7ca71a6ffaa8cbe5d90e00509dcd3493b04c
Shevchik <Shev4ik.den@gmail.com>
2013-11-12 19:05:22 +0400

    Add ability to set countdown by command

--- src/tntrun/commands/SetupCommands.java
+++ src/tntrun/commands/SetupCommands.java
@@ -295,6 +295,24 @@ public class SetupCommands implements CommandExecutor {
 			}
 			return true;
 		}
+		//set countdown
+		else if (args.length == 3 && args[1].equalsIgnoreCase("setcountdown"))
+		{
+			Arena arena = plugin.pdata.getArenaByName(args[0]);
+			if (arena != null)
+			{
+				if (arena.isArenaEnabled())
+				{
+					sender.sendMessage("Disable arena first");
+					return true;
+				}
+				arena.setCountdown(Integer.valueOf(args[2]));
+			} else
+			{
+				sender.sendMessage("Arena does not exist");
+			}
+			return true;
+		}
 		//set items rewards
 		else if (args.length == 2 && args[1].equalsIgnoreCase("setitemsrewards"))
 		{
