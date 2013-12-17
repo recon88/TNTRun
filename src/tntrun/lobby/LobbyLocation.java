06f8d8b8a66f68d6819a69da2d1cacaeb6327e74
Shevchik <Shev4ik.den@gmail.com>
2013-12-18 01:55:11 +0400

    Ooops

--- src/tntrun/lobby/LobbyLocation.java
+++ src/tntrun/lobby/LobbyLocation.java
@@ -31,7 +31,6 @@ public class LobbyLocation {
 	public LobbyLocation(String worldname, Vector vector, float yaw, float pitch)
 	{
 		this.worldname = worldname;
-		if (vector != null)
 		this.x = vector.getX();
 		this.y = vector.getY();
 		this.z = vector.getZ();
